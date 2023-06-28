b78d476c782ea539cd3cf22696b436695bf0e0e0
guardiaot <90802870+guardiaot@users.noreply.github.com>
2023-06-28 11:14:16 -0300

    26

--- src/server.js
+++ src/server.js
@@ -42,10 +42,7 @@ http.listen(process.env.PORT || 4000, function () {
     console.log("Conexao detectada....");
     let id = socket.id;
 
-    socket.on('testeUser', function(data){
-        console.log(data);
-        io.emit('teste', "data teste___" + id);
-    });
+   
             
     socket.on('disconnect', function(){
         delete usernames[socket.username];
@@ -56,4 +53,9 @@ http.listen(process.env.PORT || 4000, function () {
 
     });
 
+
+    socket.on('testeUser', function(data){
+        console.log(data);
+        io.emit('teste', "data teste___" + id);
+    });
 
