eb43bd5aabecfb53764bc06630702198a7c60b10
guardiaot <90802870+guardiaot@users.noreply.github.com>
2023-06-28 12:26:18 -0300

    feat: 35

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ var http = require("http").createServer(app);
 // include socket IO
 var io = require("socket.io")(http, {
     cors: {
-        "origin": "\*",
+        "origin": "*:*",
         "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
         "preflightContinue": false,
         "optionsSuccessStatus": 204
