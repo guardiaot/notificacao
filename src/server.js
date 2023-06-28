d5239ba5ccbfb28fc9a03bf5ca72f1148eaa2e8f
guardiaot <90802870+guardiaot@users.noreply.github.com>
2023-06-28 12:16:46 -0300

    feat: 32

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ let bodyparser  = require('body-parser');
 var cors = require('cors')
 var app = express();
 const { Interface } = require("readline");
+import http from "http";
 
 
 const { connect } = require("http2");
@@ -15,15 +16,18 @@ const { connect } = require("http2");
 app.use(cors())
 app.use(bodyparser.json())
 // create http server from express instance
-var http = require("http").createServer(app);
+var server  = http.createServer(app);
  
 // include socket IO
-var io = require("socket.io")(http, {
-    cors: {
-        "origin": "*",
-        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-        "preflightContinue": false,
-        "optionsSuccessStatus": 204
+var io = require("socket.io")(server, {
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
     }
 });
  
