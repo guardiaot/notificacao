593238f343ec95686463ebcb2caf295a431a9fc8
guardiaot <90802870+guardiaot@users.noreply.github.com>
2023-06-28 09:13:59 -0300

    14

--- src/server.js
+++ src/server.js
@@ -56,7 +56,7 @@ http.listen(process.env.PORT || 4000, function () {
     io.sockets.on("connection", function (socket) { 
     console.log("Conexao detectada....");
 
-    socket.emit('teste', 'SERVER 01', 'teste 02');
+    socket.emit('teste', 'SERVER 01 - teste', 'teste 02');
             
 
             
