8855668647926e9e2cb823fee3d16f8832d3651b
guardiaot <90802870+guardiaot@users.noreply.github.com>
2023-06-28 15:08:03 -0300

    feat: 50

--- src/server.js
+++ src/server.js
@@ -11,13 +11,7 @@ const { Server } = require("socket.io");
 const { connect } = require("http2");
 
 
-app.use(cors(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-    next();
-  }))
+app.use(cors())
 
 
 app.use(bodyparser.json())
