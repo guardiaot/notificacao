3b66474244f62b6c662ab4961d8688a9f1e7db2e
guardiaot <90802870+guardiaot@users.noreply.github.com>
2023-06-29 09:47:45 -0300

    feat: 96

--- src/server.js
+++ src/server.js
@@ -43,18 +43,8 @@ const whitelist = ['http://teste.test'];
 
 app.options('*', cors());
 
-const corsOptions = {
-  credentials: true,
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
 
-app.use(cors(corsOptions));
+app.use(cors());
    // start the HTTP server at port 4000
    server.listen(process.env.PORT || 4000, function () {
     console.log("Server started running...");
