c6b2ae26833b380f439292f4d77af389054a7f62
guardiaot <90802870+guardiaot@users.noreply.github.com>
2023-06-29 08:24:59 -0300

    feat: 82

--- src/server.js
+++ src/server.js
@@ -4,13 +4,14 @@ let bodyparser  = require('body-parser');
 
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-
-
-// create http server from express instance
-var httpServer = require("http").createServer(app);
- 
-// include socket IO
+const io = require('socket.io')(server, {
+    cors: {
+        "origin": "*",
+        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+        "preflightContinue": false,
+        "optionsSuccessStatus": 204
+    }
+});
 
  
 
@@ -19,16 +20,13 @@ let messages = [];
    
 
 io.on('connection', socket =>{
-    console.log(`Socket connected:  ${socket.id}`);
-    
+    console.log(`Socket connected:  ${socket.id}`);    
     socket.emit('previousMessages', messages);
-
     socket.on('sendMessage', data =>{
         console.log(data);
         messages.push(data);
         socket.broadcast.emit('receivedMessage', data);
     })
-
 
 })
 
